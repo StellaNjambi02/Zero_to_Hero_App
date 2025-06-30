d7bc535a9038c7b18832a0f895b9547d98464883
StellaNjambi02 <njambindungistella@gmail.com>
2025-06-30 04:37:42 +0300

    typescript error fixing

--- zero_to_hero/app/metadata.tsx
+++ zero_to_hero/app/metadata.tsx
@@ -1,5 +1,12 @@
-import { metadata } from "next";
+// import { metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Zero-to-Hero",
+//   description: "AI-powered waste management platform",
+// };
+import type { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Zero-to-Hero",
   description: "AI-powered waste management platform",
+  // other metadata properties
 };
